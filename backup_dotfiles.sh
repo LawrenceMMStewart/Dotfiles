e865fb8fc4476b8a7115f01ddbc20b3fba731fd4
LawrenceMMStewart <lawrencestewarthome@gmail.com>
2023-01-30 22:45:29 +0100

    Update : 30-01-2023 @ 22:45:29

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+## create a timestamp alias for the commit message
+timestamp() {
+  date +"%d-%m-%Y @ %T"
+}
+
 
 cp ~/.zshrc .
 cp ~/.vimrc .
 cp ~/.tmux.conf .
+
+
+git pull origin master
+git add .
+git commit -m "Update : $(timestamp)"
+git push origin master
 
 
 
